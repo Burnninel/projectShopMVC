34fe441f15a8246c9c3f4541152d74fd9a212c4a
Burnninel <79659103+Burnninel@users.noreply.github.com>
2023-08-11 22:01:28 -0300

    implementing signIn page

---
+++ assets/js/site/elementsForms.js
@@ -0,0 +1,33 @@
+function form(id, action, method, body, className) {
+    var form = `<form id="${id}" action="${action}.php" method="${method}" class="${className}"></form>`;
+
+    $('#' + body).append(form);
+};
+
+function titleForm(id, title, template) {
+    var title = `<h3 id="${id}" class="titleForm">${title}</h3>`;
+
+    $('#' + template).append(title);
+};
+
+function formBody(id, template) {
+    var form = `<div id="${id}"></div>`;
+
+    $('#' + template).append(form);
+};
+
+function btnForm(id, title, element) {
+    var btn = `<button type="submit" class="buttonForms" id="${id}">${title}</button>`;
+
+    $('#' + element).append(btn);
+};
+
+function inputsForms(idComponent, type, name, id, placeholder, element) {
+    var input = `
+        <div class="inputStyling" id="${idComponent}">
+            <input type="${type}" name="${name}" class="inputFormStatus" id="${id}" placeholder="${placeholder}">
+        </div>
+    `;
+
+    $('#' + element).append(input);
+};
---
+++ assets/js/site/signIn/signIn.js
@@ -0,0 +1,12 @@
+form('templateSignIn', '../php/validationLogin', 'post', 'signInComponent', 'templateSignIn')
+
+titleForm('titleSingIn', 'Acesse sua conta', 'templateSignIn')
+
+formBody('formSignIn', 'templateSignIn');
+
+btnForm('buttonSubmitForm', 'Entrar', 'templateSignIn');
+
+inputsForms('emailSingInContent', 'email', 'email', 'emailSingIn', 'Digite seu email', 'formSignIn');
+inputsForms('pwSingInContent', 'password', 'password', 'pwSingIn', 'Digite sua senha', 'formSignIn');
+
+console.log('ola')

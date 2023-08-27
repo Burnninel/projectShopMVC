9d46c399380ac27bc76aed797ca2882a6758362e
Burnninel <79659103+Burnninel@users.noreply.github.com>
2023-08-27 13:00:35 -0300

    svg info profile

--- views/site/account/account.php
+++ views/site/account/account.php
@@ -21,6 +21,7 @@
     <link rel="stylesheet" href="<?php echo $url; ?>/assets/css/userAccount/bodyFormAddress.css">
     <link rel="stylesheet" href="<?php echo $url; ?>/assets/css/userAccount/card.css">
     <link rel="stylesheet" href="<?php echo $url; ?>/assets/css/userAccount/loading.css">
+    <link rel="stylesheet" href="<?php echo $url; ?>/assets/css/userAccount/menusInfosAccount.css">
 
     <title>Entre ou Cadastre-se</title>
 
@@ -39,6 +40,17 @@
                     <div id="dateCreate">
                         <span id="titleDate">Entrou em:</span>
                         <span id="textDate"></span>
+                    </div>
+                    <div id="menuInfosAccount">
+                        <div class="componentSvgInfoProfile">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#fff" height="1.1rem"><path d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"/></svg>
+                        </div>
+                        <div class="componentSvgCardProfile">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#fff" height="1.1rem"><path d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z"/><path d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z" /></svg>
+                        </div>
+                        <div class="componentSvgCardProfile">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#fff" height="1.1rem"><path d="M2.25 8.25h19.5M2.25 9h19.5m-16.5 5.25h6m-6 2.25h3m-3.75 3h15a2.25 2.25 0 002.25-2.25V6.75A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25v10.5A2.25 2.25 0 004.5 19.5z"/></svg>
+                        </div>
                     </div>
                 </div>
 
@@ -83,8 +95,8 @@
     <script src="<?php echo $url; ?>/assets/js/site/userAccount/account.js"></script>
     <script src="<?php echo $url; ?>/assets/js/site/userAccount/headerProfile.js"></script>
     <script src="<?php echo $url; ?>/assets/js/site/userAccount/alterCardAddress.js"></script>
-    
-    
+
+
     <script src="<?php echo $url; ?>/assets/js/site/userAccount/address/bodyFormAddress.js"></script>
     <script src="<?php echo $url; ?>/assets/js/site/userAccount/address/cep.js"></script>
     <script src="<?php echo $url; ?>/assets/js/site/userAccount/address/validateAddress.js"></script>
@@ -96,9 +108,9 @@
     <script src="<?php echo $url; ?>/assets/js/site/userAccount/card/cardInfo.js"></script>
     <script src="<?php echo $url; ?>/assets/js/site/userAccount/card/regex.js"></script>
     <script src="<?php echo $url; ?>/assets/js/site/userAccount/card/validateCard.js"></script>
-    
+
     <script src="<?php echo $url; ?>/assets/js/site/statusForms.js"></script>
-    
+
     <script src="<?php echo $url; ?>/assets/js/site/userAccount/lenghtInputNumber.js"></script>
 
     <script src="<?php echo $url; ?>/assets/js/site/userAccount/imgProfile.js"></script>
